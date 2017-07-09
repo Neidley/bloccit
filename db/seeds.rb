fedf4d68f2c69bb7fe6eed53d42e390743c870bf
Andrew Neidley <neidz44@gmail.com>
2017-07-08 20:47:00 -0500

    Assignment 18-Rails-CRUD: in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,22 @@ require 'random_data'
 
 # Create Posts
 50.times do
-  Post.create!(
+  Advertisement.create!(
     title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
+    copy: RandomData.random_paragraph,
+    price: RandomData.random_number
   )
 end
-posts = Post.all
+advertisements = Advertisement.all
 
 # Create Comments
-100.times do
-  Comment.create!(
-  post: posts.sample,
-  body: RandomData.random_paragraph
-  )
-end
+#100.times do
+#  Comment.create!(
+#  post: posts.sample,
+#  body: RandomData.random_paragraph
+#  )
+#end
 
 puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} advertisements created"
+# puts "#{Comment.count} comments created"
