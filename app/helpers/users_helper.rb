37eb8f73b3bc3383a8d363b9b417f133c4cd715a
Andrew Neidley <neidz44@gmail.com>
2017-07-13 03:36:03 -0500

    Assignment 31-Rails-Public Profiles: completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def user_has_no_posts
+    "#{@user.name} has not submitted any posts yet."
+  end
+
+  def user_has_no_comments
+    "#{@user.name} has not submitted any comments yet."
+  end
+
 end
